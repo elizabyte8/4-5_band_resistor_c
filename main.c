69ca8efc9686714efe8b41b8569de05bdc774ffa
cold_summer <73346910+ellyzabe8@users.noreply.github.com>
2021-01-24 09:24:57 -0100

    difficult version

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 *** Title: rEsIsToЯ_bAnD_сОl0ur_CoDe__CaLcUlAt0Я ************
 *** Author: cold_summer aka Elizabeth Maslennikova **********
 *** Date: ~01/12/2019 *************************************** yes, it was written in 2019
-*** Description: Save my farm from rodents invasion *********
 ************************************************************/
 
 #include <stdio.h>
